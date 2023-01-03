e8b07b018ca1e3683e7baec6f014fdab271cf87d
LbAsHaDev_abdessamia_rguibi <abdessamia63@gmail.com>
2023-01-03 23:03:39 +0100

    file

--- code.php
+++ code.php
@@ -28,6 +28,7 @@ if (isset($_POST['export_excel_btn'])) {
     $q = $C - $D;
 
 
+
     if ($T == '1') {
         if (abs($MT) == abs($q)) {
             $query = "SELECT substring(Code_enregistrement,2,2),substring(Code_banque,1,5),substring(Code_interne,1,4), substring(Code_guichet,1,5), substring(Devise,1,3),substring(Indice_d_exaunération,1,1),substring(RIB,1,11),substring(CIB,1,2),date_format(Date_opération,'%d%m%y') as 'Date_operation' ,date_format(Date_de_valeur,'%d%m%y') as 'Date_devaleur',substring(Libellé,1,31),substring(Référence,1,11),substring(Montant,1,30),SENS FROM `EXCEL` ORDER BY substring(Code_enregistrement,1,2) ASC";
@@ -36,14 +37,25 @@ if (isset($_POST['export_excel_btn'])) {
 
                 while ($row1 = $query_run->fetch_assoc()) {
 
-                    $ff = str_pad($row1['substring(Code_enregistrement,2,2)'], 2, "0", STR_PAD_LEFT) . "" . str_pad($row1['substring(Code_banque,1,5)'], 5, "0", STR_PAD_LEFT) . "" . str_replace('0000', '    ', str_pad($row1['substring(Code_interne,1,4)'], 4, "0", STR_PAD_LEFT)) . "" . str_replace('00000', '     ', str_pad($row1['substring(Code_guichet,1,5)'], 5, "0", STR_PAD_LEFT)) . "" . str_pad($row1['substring(Devise,1,3)'], 3, " ", STR_PAD_RIGHT) . "" . str_pad($row1['substring(Indice_d_exaunération,1,1)'], 1, " ", STR_PAD_RIGHT) . " " . str_pad($row1['substring(RIB,1,11)'], 11, " ", STR_PAD_RIGHT) . "" . str_pad($row1['substring(CIB,1,2)'], 2, " ", STR_PAD_RIGHT) . "" . str_pad($row1['Date_operation'], 6, " ", STR_PAD_RIGHT) . "  " . str_replace('010170', '      ', str_pad($row1['Date_devaleur'], 6, " ", STR_PAD_RIGHT)) . "" . str_pad($row1['substring(Libellé,1,31)'], 31, " ", STR_PAD_RIGHT) . "" . str_pad($row1['substring(Référence,1,11)'], 11, " ", STR_PAD_RIGHT) . "" . $row1['substring(Montant,1,30)'] . "000402016" . str_pad($row1['SENS'], 7, " ", STR_PAD_RIGHT) . "\n";
+                    $ff = str_pad($row1['substring(Code_enregistrement,2,2)'], 2, "0", STR_PAD_RIGHT) . "" . str_pad($row1['substring(Code_banque,1,5)'], 5, "0", STR_PAD_LEFT) . "" . str_replace('0000', '    ', str_pad($row1['substring(Code_interne,1,4)'], 4, "0", STR_PAD_LEFT)) . "" . str_replace('00000', '     ', str_pad($row1['substring(Code_guichet,1,5)'], 5, "0", STR_PAD_LEFT)) . "" . str_pad($row1['substring(Devise,1,3)'], 3, " ", STR_PAD_RIGHT) . "" . str_pad($row1['substring(Indice_d_exaunération,1,1)'], 1, " ", STR_PAD_RIGHT) . " " . str_pad($row1['substring(RIB,1,11)'], 11, " ", STR_PAD_RIGHT) . "" . str_pad($row1['substring(CIB,1,2)'], 2, " ", STR_PAD_RIGHT) . "" . str_pad($row1['Date_operation'], 6, " ", STR_PAD_RIGHT) . "  " . str_replace('010170', '      ', str_pad($row1['Date_devaleur'], 6, " ", STR_PAD_RIGHT)) . "" . str_pad($row1['substring(Libellé,1,31)'], 31, " ", STR_PAD_RIGHT) . "" . str_pad($row1['substring(Référence,1,11)'], 11, " ", STR_PAD_RIGHT) . "" . $row1['substring(Montant,1,30)'] . "000402016" . str_pad($row1['SENS'], 7, " ", STR_PAD_RIGHT) . "\n";
 
 
-                    header("Content-Type: text/plain");
-                    header('Content-Disposition: attachement; filename="data.txt"');
 
-                    print($ff);
+                    // header("Content-Type: text/plain; charset=utf-8");
+                    // header('Content-Disposition: attachement; filename="data.txt"');
+
+                    // echo ($ff);
+
+                    $name = date("YmdHi");
+                    // $f = $name . "" . '.txt';
+                    $prt = fopen("C:\Users\Lenovo Thinkpad X280\Downloads\data$name.txt", "a+");
+                    fwrite($prt, $ff);
+                    fclose($prt);
+                    $_SESSION['message'] = "Upload successfully";
+                    header('Location: index.php');
                 }
+
+                // file_put_contents($prt, $ff);
             }
         } else {
             $_SESSION['message'] = " Invalid Amount'($MT)' Upload The Correct file ";
